b2dcd403254e717b2a6e6283a6acd54a2d3d35b4
Alex010011 <bunkof3@yandex.ru>
2022-01-08 13:43:15 +0200

    Update test_1.java

--- src/test/java/firstProject/test_1.java
+++ src/test/java/firstProject/test_1.java
@@ -21,21 +21,47 @@ public class test_1 {
     }
 
     @Test
-    public void testName1(){
+    public void testNew() {
 
         WebElement el1 = wd.findElement(By.tagName("a"));
         WebElement el2 = wd.findElement(By.tagName("div"));
-        //WebElement el3 = wd.findElement(By.className("btn btn-sm btm-link"));
-        //WebElement link = wd.findElement(By.linkText("Sign Up"));
+        WebElement el22 = wd.findElement(By.cssSelector("div"));
+        WebElement el5 = wd.findElement(By.id("hero"));
+        WebElement el52 = wd.findElement(By.cssSelector("#hero"));
+        WebElement el3 = wd.findElement(By.className("font-weight-bold"));
+        WebElement el33 = wd.findElement(By.cssSelector(".font-weight-bold"));
+
+        //WebElement link1= wd.findElement(By.linkText("Sign Up"));
+        WebElement link11= wd.findElement(By.cssSelector("[href='/login']"));
+
         //WebElement partLink = wd.findElements(By.partialLinkText("Sign"));
+    }
+    @Test
+    public void test2(){
+
         List<WebElement> ael = wd.findElements(By.tagName("a"));
         System.out.println(ael.size());
+
+
+    }
+
+    @Test
+    public void enterEmail(){
+
+        WebElement el4 = wd.findElement(By.cssSelector("input[name='email']"));
+        type(el4, "bunkof3@yandex.ru");
+
+    }
+
+    public void type(WebElement el4, String email) {
+        el4.click();
+        el4.clear();
+        el4.sendKeys(email);
     }
 
     @AfterMethod
 
     public void close(){
-
         //wd.close();
         //wd.quit();
     }
